5a5f1a58e603fa2b5f5c4211141504a54d71405f
Brandelyn Duenckel <brandelyns@gmail.com>
2021-01-24 22:51:07 -0700

    added first commit

---
+++ src/codingWeek2/week2Notes.java
@@ -0,0 +1,11 @@
+package codingWeek2;
+
+public class week2Notes {
+
+	public static void main(String[] args) {
+		int age = 14;
+		System.out.println(age);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module codingWeek2 {
+}
